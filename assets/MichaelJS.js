4767dca9367c5ced2fd969b5cc438fb6d65cae1b
sticks1988 <mikepb6691@aol.com>
2021-09-02 21:13:56 -0400

    functions to JS
    adding functions to JS for API results to load

--- assets/MichaelJS.js
+++ assets/MichaelJS.js
@@ -1,3 +1,28 @@
-function lengthSelect() {
-var lengthdropdown = document.getElementById("lengthdropdown");
+var button = document.getElementById('lengthdropdown');
+button.addEventListener('click', searchMovieLength);
+
+function searchMovieLength() {
+  var userInputValue = document.getElementById('lengthdropdown').value;
+  var urlBase = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup'
+  var searchUrl = urlBase + userInputValue;
+  performSearch(searchUrl);
+}
+
+function performSearch(searchUrl) {
+  var requestData = new XMLHttpRequest();
+  requestData.addEventListener('load', lengthResults);
+  requestData.open('GET', searchUrl);
+  requestData.send();
+}
+
+function lengthResults() {
+  var responseJSON = JSON.parse(this.response);
+  if (responseJSON.error) console.log('Character not found');
+  else {
+    var urlBase = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup';
+    responseJSON.results.forEach(function (value)  {
+    const value = document.getElementById('mySearch').value;
+    })
+    document.getElementById('demo').innerHTML = urlBase;
+  }
 }
