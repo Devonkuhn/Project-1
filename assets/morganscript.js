c2aaa3e09ac130ec3ed40f99b75a8b3b2bd45391
morganmmoore <morganmmoore0@gmail.com>
2021-09-08 18:29:47 -0400

    morgan's updated javascript

--- assets/morganscript.js
+++ assets/morganscript.js
@@ -1,25 +1,120 @@
 var genreBtn = document.getElementById("genreBtn");
+var apiKey = '9f1f886920cacc5ab769ccbe0ad1489f';
+var genre = document.getElementById('genreDropdown').value;
+var apiUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=' + apiKey + '&language=en-US&sort_by=popularity.desc&with_genres=' + genre;
+var data = '';
+var genreID ='';
+var genreList = [
+  {
+    name: 'Action',
+    id: '28'
+  },
+  {
+    name: 'Comedy',
+    id: '35'
+  },
+  {
+    name: 'Drama',
+    id: '18'
+  },
+  {
+    name: 'Fantasy',
+    id: '14'
+  },
+  {
+    name: 'Horror',
+    id: '27'
+  },
+  {
+    name: 'Mystery',
+    id: '9648'
+  },
+  {
+    name: 'Romance',
+    id: '10749'
+  },
+  {
+    name: 'Thriller',
+    id: '53'
+  }
+];
 
-var getGenre = function(genre) {
-
-    var apiURL = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup';
-    var userInputValue = document.getElementById('genreDropdown').value;
-    var searchURL = apiURL + userInputValue;
-
-    displayGenre(searchURL);
-
+function searchGenre() {
+    genre = document.getElementById('genreDropdown').value;
+    console.log(genre);
+    getGenreList();
+    getGenre(genre);
 }
 
-var displayGenre = function(searchURL) {
-    fetch(apiURL).then(function (response) {    
-    if (response.status !== 200) {
-
-    } 
-    response.json().then(function (data) {
-        console.log(data);
-        displayGenre(data);
-        });
+function getGenreList() {
+    fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + apiKey + '&language=en-US', {
+        headers: { 
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOGI4NDBhOTQwMjRkYjkyN2UwNGQ3NDI5MDc5YThhMyIsInN1YiI6IjYxMzQyY2FhMGI3MzE2MDAyYWM2YWFhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.58X4jp1eA5k7TlNbmZxRphw11xdo28XelQE9Sd-DGLM'
+      }})
+    .then(function(response) {
+        console.log(response.status);
+        return response.json();
     })
+    .then(function(data) {
+        console.log(data);
+    })
+    for (i = 0; i < genre.length; i++) {
+      if (genre == genreList[i].name) {
+        genreID = genreList[i].id
+        break;
+      }
+    }
 }
 
-genreBtn.addEventListener('submit', displayGenre);
+function getGenre(genre) {
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=' + apiKey + '&with_genres=' + genreID, {
+      headers: { 
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOGI4NDBhOTQwMjRkYjkyN2UwNGQ3NDI5MDc5YThhMyIsInN1YiI6IjYxMzQyY2FhMGI3MzE2MDAyYWM2YWFhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.58X4jp1eA5k7TlNbmZxRphw11xdo28XelQE9Sd-DGLM'
+    }})
+      .then(function (response) {
+        console.log(response.status);
+        if (response.status !== 200) {
+          responseText.textContent = response.status;
+        }
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        displaySearch (data, false);
+        localStorage.lengthResults = JSON.stringify(data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  
+  }
+
+var displayGenre = function(data, historic) {
+    var html = "";
+
+  if (historic == true) {
+    html+="<strong><em>Previous Results</em></strong><br>";
+  }
+
+  else if (historic == false) {
+    html+="<strong><em>Search Results</em></strong><br>"
+  }
+  
+  for (let i = 0; i < data.results.length && i < 15; i++) {
+
+    var movie = data.results [i];
+    html += movie.original_title + "<br>";
+  }
+
+  var genreResultsDiv = document.getElementById('results');
+  genreResultsDiv.innerHTML = html
+}
+
+genreBtn.addEventListener('click', searchGenre);
+
+window.addEventListener('load', function() {
+    if (localStorage.genreResults) {
+        data = JSON.parse(localStorage.genreResults);
+        displayGenre(data, true);
+    }
+})
