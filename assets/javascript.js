d5f887dde4ed128291167b50c6bf5163e08e21ed
sticks1988 <mikepb6691@aol.com>
2021-09-10 01:16:38 -0400

    fixed javascript
    fixed javascript with functions.
    
    fixed CSS modal to where it closes and the page is functional.
    
    made it where there is only one window load event that calls multiple functions.

--- assets/javascript.js
+++ assets/javascript.js
@@ -4,23 +4,15 @@ var button = document.getElementById("yearButton");
 button.addEventListener("click", searchreleaseYear);
 
 function searchreleaseYear() {
-<<<<<<< HEAD
-  var length = document.getElementById("yearInput").value;
-=======
-  var length = document.getElementById("yearButton").value;
->>>>>>> 3182fe9573c2880d6cf204564f5a383b5955ff49
+  var year = document.getElementById("yearSelect").value;
 
-  performSearch(length);
+  performSearch(year);
 }
 
-function performSearch(length) {
+function performSearch(year) {
   fetch(
-<<<<<<< HEAD
-    "https://developers.themoviedb.org/3/discover/movie?sort_by&primary_release_date.gte=2-15-01-01&primary_release_date.lte=2020-01-01" +
-=======
-    "https://developers.themoviedb.org/3/discover/movie?sort_by&primary_release_date.gte=2000-15-01-01&primary_release_date.lte=2020-09-09" +
->>>>>>> 3182fe9573c2880d6cf204564f5a383b5955ff49
-      length,
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&sort_by&primary_release_year=" +
+      year,
     {
       headers: {
         Authorization:
@@ -38,7 +30,7 @@ function performSearch(length) {
     .then(function (data) {
       console.log(data);
       displaySearch(data, false);
-      localStorage.lengthResults = JSON.stringify(data);
+      localStorage.yearResults = JSON.stringify(data);
     })
     .catch((error) => {
       console.error("Error:", error);
@@ -52,35 +44,9 @@ function displaySearch(data, historic) {
     html += "<strong><em>Previous Results</em></strong><br>";
   } else if (historic == false) {
     html += "<strong><em>Search Results</em></strong><br>";
-<<<<<<< HEAD
   }
 
   for (let i = 0; i < data.results.length && i < 15; i++) {
-    var movie = data.results[i];
-    html += movie.original_title + "<br>";
-  }
-
-  var resultsDiv = document.getElementById("results");
-  resultsDiv.innerHTML = html;
-}
-
-window.addEventListener("load", function () {
-  if (localStorage.lengthResults) {
-    var data = JSON.parse(localStorage.lengthResults);
-    displaySearch(data, true);
-  }
-});
-
-//modal
-const modalBg = document.querySelector(".modal-background");
-const modal = document.querySelector(".modal");
-
-modal.addEventListener("click", () => {
-  modal.classList.remove("is-active");
-=======
-  }
-
-  for (let i = 0; i < data.results.yearResults && i < 15; i++) {
     var movie = data.results[i];
     html += movie.original_title + "<br>";
   }
@@ -89,10 +55,9 @@ modal.addEventListener("click", () => {
   resultsDiv.innerHTML = html;
 }
 
-window.addEventListener("load", function () {
+function initYearResults() {
   if (localStorage.yearResults) {
     var data = JSON.parse(localStorage.yearResults);
     displaySearch(data, true);
   }
->>>>>>> 3182fe9573c2880d6cf204564f5a383b5955ff49
-});
+}
